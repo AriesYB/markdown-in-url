7e27bf3cc89ac1d0be0f4a8c18be52515611a009
HetFrame <37505429+AriesYB@users.noreply.github.com>
2026-02-05 15:28:25 +0800

    +:github pages配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // GitHub Pages 部署配置：设置为仓库名称
+  base: '/markdown-in-url/',
+});
